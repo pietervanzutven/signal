09f26c1dd63440e7935662eb45728cec12889956
Pieter van Zutven <pietervanzutven@outlook.com>
2020-02-18 18:03:37 +0100

    Additional logging to help track down socket disconnect problems

--- Signal/libtextsecure/websocket-resources.js
+++ Signal/libtextsecure/websocket-resources.js
@@ -174,6 +174,7 @@
             clearTimeout(this.keepAliveTimer);
             clearTimeout(this.disconnectTimer);
             this.keepAliveTimer = setTimeout(function() {
+                console.log('Sending a keepalive message');
                 this.wsr.sendRequest({
                     verb: 'GET',
                     path: this.path,
