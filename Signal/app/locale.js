de9fc52c60befdcfd466ba771d2a8ff4dcb033ff
Pieter van Zutven <pietervanzutven@outlook.com>
2020-03-15 17:10:08 +0100

    Refactor locale to only expose the data we care about

--- Signal/app/locale.js
+++ Signal/app/locale.js
@@ -1,13 +1,13 @@
-﻿locale = {
-    normalizeLocaleName: function (locale) {
+﻿(function () {
+    function normalizeLocaleName(locale) {
         if (/^en-/.test(locale)) {
             return 'en';
         }
 
         return locale;
-    },
+    }
 
-    getLocaleMessages: function (locale) {
+    function getLocaleMessages(locale) {
         const onDiskLocale = locale.replace('-', '_');
         const targetFile = '_locales/' + onDiskLocale + '/messages.json';
 
@@ -17,4 +17,24 @@
 
         return JSON.parse(xhr.response);
     }
-}
+
+    // Load locale - if we can't load messages for the current locale, we
+    // default to 'en'
+    //
+    // possible locales:
+    // https://github.com/electron/electron/blob/master/docs/api/locales.md
+    let localeName = normalizeLocaleName(Windows.Globalization.ApplicationLanguages.languages[0]);
+    let messages;
+
+    try {
+        messages = getLocaleMessages(localeName);
+    } catch (e) {
+        console.log('Problem loading messages for locale ', localeName, e.stack);
+        console.log('Falling back to en locale');
+
+        localeName = 'en';
+        messages = getLocaleMessages(localeName);
+    }
+
+    window.locale = { messages: messages };
+})()
--- Signal/preload.js
+++ Signal/preload.js
@@ -4,24 +4,7 @@ console.log('preload');
 
 window.PROTO_ROOT = '/protos';
 
-// Load locale - if we can't load messages for the current locale, we
-// default to 'en'
-//
-// possible locales:
-// https://github.com/electron/electron/blob/master/docs/api/locales.md
-let localeName = locale.normalizeLocaleName(Windows.Globalization.ApplicationLanguages.languages[0]);
-let messages;
-
-try {
-    messages = locale.getLocaleMessages(localeName);
-} catch (e) {
-    console.log('Problem loading messages for locale ', localeName, e.stack);
-    console.log('Falling back to en locale');
-
-    localeName = 'en';
-    messages = locale.getLocaleMessages(localeName);
-}
-window.config.localeMessages = messages;
+window.config.localeMessages = locale.messages;
 
 var Notifications = Windows.UI.Notifications;
 window.setBadgeCount = function (count) {
