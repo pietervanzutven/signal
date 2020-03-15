016b5d439e6123dec265591a16616f7d44c21818
Pieter van Zutven <pietervanzutven@outlook.com>
2020-03-15 16:59:12 +0100

    Refactor configuration out into reusable files

---
+++ Signal/app/config.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    const environment = 'production';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'config/default.json', false);
+    xhr.send(null);
+
+    window.config = JSON.parse(xhr.response);
+
+    xhr.open('GET', 'config/' + environment + '.json', false);
+    xhr.send(null);
+
+    window.config = Object.assign(window.config, JSON.parse(xhr.response));
+    window.config.environment = environment;
+})()
