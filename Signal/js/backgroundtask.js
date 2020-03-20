e489bd41056da0df3173b0c75e83b1fcf5433e66
Pieter van Zutven <pietervanzutven@outlook.com>
2020-03-20 20:58:18 +0100

    Restructure background task

--- Signal/js/backgroundtask.js
+++ Signal/js/backgroundtask.js
@@ -1,13 +1,14 @@
 ﻿'use strict';
 
-var Notifications = Windows.UI.Notifications;
-
 var window = this;
 window.config = {};
 var loadLocale = function () { return { messages: {} } };
 
 importScripts('ms-appx:///libtextsecure/components.js', 'ms-appx:///preload.js', 'ms-appx:///libtextsecure/event_target.js', 'ms-appx:///libtextsecure/protobufs.js', 'ms-appx:///libtextsecure/websocket-resources.js');
 
+var Notifications = Windows.UI.Notifications;
+Notifications.ToastNotificationManager.history.clear();
+window.setBadgeCount(0);
 function updateToast(message) {
     var toastXml = Notifications.ToastNotificationManager.getTemplateContent(Notifications.ToastTemplateType.toastText02);
     var toastNodeList = toastXml.getElementsByTagName('text');
@@ -17,32 +18,27 @@ function updateToast(message) {
     Notifications.ToastNotificationManager.createToastNotifier().show(toast);
 }
 
-(function () {
-    Notifications.ToastNotificationManager.history.clear();
-    window.setBadgeCount(0);
+var url = 'https://textsecure-service.whispersystems.org';
+var number_id = Windows.Storage.ApplicationData.current.localSettings.values['number_id'];
+var password = Windows.Storage.ApplicationData.current.localSettings.values['password'];
 
-    var url = 'https://textsecure-service.whispersystems.org';
-    var number_id = Windows.Storage.ApplicationData.current.localSettings.values['number_id'];
-    var password = Windows.Storage.ApplicationData.current.localSettings.values['password'];
+var socket = new WebSocket(
+url.replace('https://', 'wss://').replace('http://', 'ws://')
+    + '/v1/websocket/?login=' + encodeURIComponent(number_id)
+    + '&password=' + encodeURIComponent(password)
+    + '&agent=OWD');
 
-    var socket = new WebSocket(
-    url.replace('https://', 'wss://').replace('http://', 'ws://')
-        + '/v1/websocket/?login=' + encodeURIComponent(number_id)
-        + '&password=' + encodeURIComponent(password)
-        + '&agent=OWD');
+socket.onclose = () => close();
 
-    socket.onclose = () => close();
-
-    var wsr = new WebSocketResource(socket, {
-        handleRequest: request => {
-            if (request.path === '/api/v1/message' && Notifications.ToastNotificationManager.history.getHistory().length < 1) {
-                updateToast('New message(s) received');
-                window.setBadgeCount('newMessage');
-            } else {
-                request.respond(200, 'OK');
-            }
-            socket.close();
-        },
-        keepalive: { path: '/v1/keepalive', disconnect: true }
-    });
-})();
+var wsr = new WebSocketResource(socket, {
+    handleRequest: request => {
+        if (request.path === '/api/v1/message' && Notifications.ToastNotificationManager.history.getHistory().length < 1) {
+            updateToast('New message(s) received');
+            window.setBadgeCount('newMessage');
+        } else {
+            request.respond(200, 'OK');
+        }
+        socket.close();
+    },
+    keepalive: { path: '/v1/keepalive', disconnect: true }
+});
