39e82ed44cd8f121e83df5e78782fad0a284ee24
Pieter van Zutven <pietervanzutven@outlook.com>
2020-04-13 16:23:03 +0200

    Key rotation: force on new version

--- Signal/js/background.js
+++ Signal/js/background.js
@@ -96,6 +96,7 @@
 
         if (!lastVersion || currentVersion !== lastVersion) {
             console.log('New version detected:', currentVersion);
+            getAccountManager().rotateSignedPreKey();
         }
 
         window.dispatchEvent(new Event('storage_ready'));
