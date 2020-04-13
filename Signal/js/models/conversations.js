f14a27d047a51710226c65773922089ecfe8d6ba
Pieter van Zutven <pietervanzutven@outlook.com>
2020-04-13 12:09:53 +0200

    Remove conversation from left pane on 'delete messages'

--- Signal/js/models/conversations.js
+++ Signal/js/models/conversations.js
@@ -1001,8 +1001,14 @@
         }).then(function() {
             var models = this.messageCollection.models;
             this.messageCollection.reset([]);
-            _.each(models, function(message) { message.destroy(); });
-            this.save({lastMessage: null, timestamp: null}); // archive
+            _.each(models, function(message) {
+                message.destroy();
+            });
+            this.save({
+                lastMessage: null,
+                timestamp: null,
+                active_at: null,
+            });
         }.bind(this));
     },
 
