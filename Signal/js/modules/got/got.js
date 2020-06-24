33590346d69f2652582be19f88f09b8b41fbfdd9
Pieter van Zutven <pietervanzutven@outlook.com>
2020-06-24 20:23:13 +0200

    Add dependency for custom implementation of `got`

---
+++ Signal/js/modules/got/got.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    window.got = {
+        get: async url => {
+            var httpClient = Windows.Web.Http.HttpClient();
+            var httpResponse = await httpClient.getAsync(new Windows.Foundation.Uri(url));
+            var response = await httpResponse.content.readAsStringAsync();
+            return {body: JSON.parse(response)};
+        }
+    }
+})()
