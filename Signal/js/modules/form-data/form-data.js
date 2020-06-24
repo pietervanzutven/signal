a38962476735de14198f988420318ec2bdddeb19
Pieter van Zutven <pietervanzutven@outlook.com>
2020-06-24 20:23:50 +0200

    Add dependency for custom implementation of `form-data`

---
+++ Signal/js/modules/form-data/form-data.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    window.form_data = function () {
+        this.formData = Windows.Web.Http.HttpMultipartFormDataContent();
+        this.append = function (key, value) {
+            var httpContent = Windows.Web.Http.HttpStringContent(value);
+            this.formData.add(httpContent);
+        }
+        this.submit = async function (url) {
+            var httpClient = Windows.Web.Http.HttpClient();
+            response = await httpClient.postAsync(new Windows.Foundation.Uri(url), this.formData);
+        }
+    }
+})();
