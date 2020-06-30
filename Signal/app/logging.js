00cdffe52413d3292520ab686b421bbed71c7c9a
Pieter van Zutven <pietervanzutven@outlook.com>
2020-06-30 20:51:00 +0200

    Use proper logging if error pops when deleting all logs

--- Signal/app/logging.js
+++ Signal/app/logging.js
@@ -53,8 +53,8 @@
       ipc.on('delete-all-logs', async (event) => {
         try {
           await deleteAllLogs(logPath);
-        } catch (e) {
-          console.log('Something went wrong!');
+        } catch (error) {
+          logger.error(`Problem deleting all logs: ${error.stack}`);
         }
 
         event.sender.send('delete-all-logs-complete');
