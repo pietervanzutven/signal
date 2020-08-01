a8fdb7e299cbf0167061188b730bfa08eb24d294
Pieter van Zutven <pietervanzutven@outlook.com>
2020-08-01 17:58:05 +0200

    Refactor backup.js to use async/await

--- Signal/js/backup.js
+++ Signal/js/backup.js
@@ -7,7 +7,7 @@
 
 /* eslint-env node */
 
-/* eslint-disable no-param-reassign, more/no-then, guard-for-in */
+/* eslint-disable no-param-reassign, guard-for-in */
 
 'use strict';
 
@@ -15,13 +15,13 @@
 (function() {
   window.Whisper = window.Whisper || {};
 
-  function exportDatabase(idb_db, parent) {
+  async function exportDatabase(idb_db, parent) {
     const promises = [];
     idb_db.files.forEach(file => promises.push(file.copyAsync(parent, file.name, Windows.Storage.NameCollisionOption.replaceExisting)));
     return Promise.all(promises);
   }
 
-  function importDatabase(idb_db, parent) {
+  async function importDatabase(idb_db, parent) {
     const promises = [];
     parent.files.forEach(file => {
       var fileName = file.name === 'signal.json' ? 'signal_import.json' : file.name;
@@ -30,19 +30,21 @@
     return Promise.all(promises);
   }
 
-  function openDatabase() {
+  async function openDatabase() {
     const folder = Windows.Storage.ApplicationData.current.localFolder;
-    return folder.getFilesAsync().then(function (files) {
-      return { folder: folder, files: files }
-    });
+    return folder.getFilesAsync()
+      .then(files => {
+        return { folder: folder, files: files }
+      });
   }
 
   function createDirectory(parent, name) {
     const sanitized = sanitizeFileName(name);
     console._log('-- about to create directory', sanitized);
-    return parent.folder.createFolderAsync(name, Windows.Storage.CreationCollisionOption.replaceExisting).then(function (folder) {
-      return folder;
-    });
+    return parent.folder.createFolderAsync(name, Windows.Storage.CreationCollisionOption.replaceExisting)
+      .then(folder => {
+        return folder;
+      });
   }
 
   function sanitizeFileName(filename) {
@@ -54,23 +56,25 @@
       var picker = Windows.Storage.Pickers.FolderPicker();
       picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
       picker.fileTypeFilter.append("*");
-      picker.pickSingleFolderAsync().then(function(folder) {
-        if (folder) {
+
+      picker.pickSingleFolderAsync()
+        .then(folder => {
+          if (!folder) {
+            const error = new Error('Error choosing directory');
+            error.name = 'ChooseError';
+            return reject(error);
+          }
+
           return folder.getFilesAsync()
             .then(files => {
               return resolve({ folder: folder, files: files })
             }, error => {
               return reject(error)
             });
-        } else {
-          const error = new Error('Error choosing directory');
-          error.name = 'ChooseError';
-          return reject(error);
-        }
-      });
+        });
     });
   }
- 
+
   function getTimestamp() {
     return moment().format('YYYY MMM Do [at] h.mm.ss a');
   }
@@ -84,27 +88,22 @@
       };
       return getDirectory(options);
     },
-    exportToDirectory(directory, options) {
-      let dir;
-      let db;
-      return openDatabase().then((openedDb) => {
-        db = openedDb;
-        const name = `Signal Export ${getTimestamp()}`;
-        return createDirectory(directory, name);
-      }).then((created) => {
-        dir = created;
-        return exportDatabase(db, dir, options);
-      }).then(() => dir.path)
-        .then((targetPath) => {
-          console.log('done backing up!');
-          return targetPath;
-        }, (error) => {
-          console.log(
-            'the backup went wrong:',
-            error && error.stack ? error.stack : error
-          );
-          return Promise.reject(error);
-        });
+    async exportToDirectory(directory, options) {
+      const name = `Signal Export ${getTimestamp()}`;
+      try {
+        const db = await openDatabase();
+        const dir = await createDirectory(directory, name);
+        await exportDatabase(db, dir, options);
+
+        console.log('done backing up!');
+        return dir.path;
+      } catch (error) {
+        console.log(
+          'the backup went wrong:',
+          error && error.stack ? error.stack : error
+        );
+        throw error;
+      }
     },
     getDirectoryForImport() {
       const options = {
@@ -113,29 +112,22 @@
       };
       return getDirectory(options);
     },
-    importFromDirectory(directory, options) {
+    async importFromDirectory(directory, options) {
       options = options || {};
 
-      let db;
-      let nonMessageResult;
-      return openDatabase().then((createdDb) => {
-        db = createdDb;
+      try {
+        const db = await openDatabase();
 
-        return importDatabase(idb_db, directory);
-      }).then(() => directory)
-        .then((result) => {
-          nonMessageResult = result;
-        })
-        .then(() => {
-          console.log('done restoring from backup!');
-          return nonMessageResult;
-        }, (error) => {
-          console.log(
-            'the import went wrong:',
-            error && error.stack ? error.stack : error
-          );
-          return Promise.reject(error);
-        });
+        const result = await importDatabase(db, directory, options);
+        console.log('done restoring from backup!');
+        return result;
+      } catch (error) {
+        console.log(
+          'the import went wrong:',
+          error && error.stack ? error.stack : error
+        );
+        throw error;
+      }
     },
   };
 }());
