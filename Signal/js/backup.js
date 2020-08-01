23c1453712cbd8292f8d0dd17a651d3c8fa15d74
Pieter van Zutven <pietervanzutven@outlook.com>
2020-08-01 17:17:16 +0200

    Code formatting updates for backup.js

--- Signal/js/backup.js
+++ Signal/js/backup.js
@@ -1,124 +1,141 @@
-;(function () {
-    'use strict';
-    window.Whisper = window.Whisper || {};
+/* global dcodeIO: false */
+/* global _: false */
+/* global Whisper: false */
+/* global textsecure: false */
+/* global moment: false */
+/* global i18n: false */
 
-    function exportDatabase(idb_db, parent) {
-        var promises = [];
-        idb_db.files.forEach(file => promises.push(file.copyAsync(parent, file.name, Windows.Storage.NameCollisionOption.replaceExisting)));
-        return Promise.all(promises);
-    }
+/* eslint-env node */
 
-    function importDatabase(idb_db, parent) {
-        var promises = [];
-        parent.files.forEach(file => {
-            var fileName = file.name === 'signal.json' ? 'signal_import.json' : file.name;
-            promises.push(file.copyAsync(idb_db.folder, fileName, Windows.Storage.NameCollisionOption.replaceExisting));
-        });
-        return Promise.all(promises);
-    }
+/* eslint-disable no-param-reassign, more/no-then, guard-for-in */
 
-    function openDatabase() {
-        var folder = Windows.Storage.ApplicationData.current.localFolder;
-        return folder.getFilesAsync().then(function (files) {
-            return { folder: folder, files: files }
-        });
-    }
+'use strict';
 
-    function createDirectory(parent, name) {
-        var sanitized = sanitizeFileName(name);
-        console._log('-- about to create directory', sanitized);
-        return parent.folder.createFolderAsync(name, Windows.Storage.CreationCollisionOption.replaceExisting).then(function (folder) {
-            return folder;
-        });
-    }
+// eslint-disable-next-line func-names
+(function() {
+  window.Whisper = window.Whisper || {};
 
-    function sanitizeFileName(filename) {
-        return filename.toString().replace(/[^a-z0-9.,+()'#\- ]/gi, '_');
-    }
+  function exportDatabase(idb_db, parent) {
+    const promises = [];
+    idb_db.files.forEach(file => promises.push(file.copyAsync(parent, file.name, Windows.Storage.NameCollisionOption.replaceExisting)));
+    return Promise.all(promises);
+  }
 
-    function getDirectory() {
-        return new Promise(function(resolve, reject) {
-            var picker = Windows.Storage.Pickers.FolderPicker();
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
-            picker.fileTypeFilter.append("*");
-            picker.pickSingleFolderAsync().then(function(folder) {
-                if (folder) {
-                    folder.getFilesAsync().then(files => resolve({ folder: folder, files: files }), error => reject(error));
-                } else {
-                    var error = new Error('Error choosing directory');
-                    error.name = 'ChooseError';
-                    reject(error);
-                }
+  function importDatabase(idb_db, parent) {
+    const promises = [];
+    parent.files.forEach(file => {
+      var fileName = file.name === 'signal.json' ? 'signal_import.json' : file.name;
+      promises.push(file.copyAsync(idb_db.folder, fileName, Windows.Storage.NameCollisionOption.replaceExisting));
+    });
+    return Promise.all(promises);
+  }
+
+  function openDatabase() {
+    const folder = Windows.Storage.ApplicationData.current.localFolder;
+    return folder.getFilesAsync().then(function (files) {
+      return { folder: folder, files: files }
+    });
+  }
+
+  function createDirectory(parent, name) {
+    const sanitized = sanitizeFileName(name);
+    console._log('-- about to create directory', sanitized);
+    return parent.folder.createFolderAsync(name, Windows.Storage.CreationCollisionOption.replaceExisting).then(function (folder) {
+      return folder;
+    });
+  }
+
+  function sanitizeFileName(filename) {
+    return filename.toString().replace(/[^a-z0-9.,+()'#\- ]/gi, '_');
+  }
+
+  function getDirectory(options) {
+    return new Promise(function(resolve, reject) {
+      var picker = Windows.Storage.Pickers.FolderPicker();
+      picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+      picker.fileTypeFilter.append("*");
+      picker.pickSingleFolderAsync().then(function(folder) {
+        if (folder) {
+          return folder.getFilesAsync()
+            .then(files => {
+              return resolve({ folder: folder, files: files })
+            }, error => {
+              return reject(error)
             });
-        });
-    }
-
-    function getDisplayPath(entry) {
-        return new Promise(function(resolve) {
-            resolve(entry.path);
-        });
-    }
-
-    function getTimestamp() {
-        return moment().format('YYYY MMM Do [at] h.mm.ss a');
-    }
-
-    Whisper.Backup = {
-        getDirectoryForExport: function() {
-            var options = {
-                title: i18n('exportChooserTitle'),
-                buttonLabel: i18n('exportButton'),
-            };
-            return getDirectory(options);
-        },
-        backupToDirectory: function (directory) {
-            var idb;
-            var dir;
-            return openDatabase().then(function (idb_db) {
-                idb = idb_db;
-                var name = 'Signal Export ' + getTimestamp();
-                return createDirectory(directory, name);
-            }).then(function (created) {
-                dir = created;
-                return exportDatabase(idb, dir);
-            }).then(function () {
-                return dir.path;
-            }).then(function (path) {
-                console.log('done backing up!');
-                return path;
-            }, function (error) {
-                console.log(
-                  'the backup went wrong:',
-                  error && error.stack ? error.stack : error
-                );
-                return Promise.reject(error);
-            });
-        },
-        getDirectoryForImport: function() {
-            var options = {
-                title: i18n('importChooserTitle'),
-                buttonLabel: i18n('importButton'),
-            };
-            return getDirectory(options);
-        },
-        importFromDirectory: function(directory) {
-            var idb;
-            return openDatabase().then(function(idb_db) {
-                idb = idb_db;
-                return importDatabase(idb_db, directory);
-            }).then(function() {
-                return directory;
-            }).then(function(path) {
-                console.log('done restoring from backup!');
-                return path;
-            }, function(error) {
-                console.log(
-                  'the import went wrong:',
-                  error && error.stack ? error.stack : error
-                );
-                return Promise.reject(error);
-            });
+        } else {
+          const error = new Error('Error choosing directory');
+          error.name = 'ChooseError';
+          return reject(error);
         }
-    };
+      });
+    });
+  }
+ 
+  function getTimestamp() {
+    return moment().format('YYYY MMM Do [at] h.mm.ss a');
+  }
 
+  // directories returned and taken by backup/import are all string paths
+  Whisper.Backup = {
+    getDirectoryForExport() {
+      const options = {
+        title: i18n('exportChooserTitle'),
+        buttonLabel: i18n('exportButton'),
+      };
+      return getDirectory(options);
+    },
+    exportToDirectory(directory, options) {
+      let dir;
+      let db;
+      return openDatabase().then((openedDb) => {
+        db = openedDb;
+        const name = `Signal Export ${getTimestamp()}`;
+        return createDirectory(directory, name);
+      }).then((created) => {
+        dir = created;
+        return exportDatabase(db, dir, options);
+      }).then(() => dir.path)
+        .then((targetPath) => {
+          console.log('done backing up!');
+          return targetPath;
+        }, (error) => {
+          console.log(
+            'the backup went wrong:',
+            error && error.stack ? error.stack : error
+          );
+          return Promise.reject(error);
+        });
+    },
+    getDirectoryForImport() {
+      const options = {
+        title: i18n('importChooserTitle'),
+        buttonLabel: i18n('importButton'),
+      };
+      return getDirectory(options);
+    },
+    importFromDirectory(directory, options) {
+      options = options || {};
+
+      let db;
+      let nonMessageResult;
+      return openDatabase().then((createdDb) => {
+        db = createdDb;
+
+        return importDatabase(idb_db, directory);
+      }).then(() => directory)
+        .then((result) => {
+          nonMessageResult = result;
+        })
+        .then(() => {
+          console.log('done restoring from backup!');
+          return nonMessageResult;
+        }, (error) => {
+          console.log(
+            'the import went wrong:',
+            error && error.stack ? error.stack : error
+          );
+          return Promise.reject(error);
+        });
+    },
+  };
 }());
