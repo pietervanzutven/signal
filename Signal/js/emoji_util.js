56f5687621c09bf1368ada550711155e715bb0c5
Pieter van Zutven <pietervanzutven@outlook.com>
2020-08-22 16:36:44 +0200

    Send quoted replies and message/quote visual updates

--- Signal/js/emoji_util.js
+++ Signal/js/emoji_util.js
@@ -4,8 +4,10 @@
 
 ; (function () {
     'use strict';
+
+    window.emoji = window.emoji || {};
     window.emoji_util = window.emoji_util || {};
 
+    window.emoji.signalReplace = str => str;
     window.emoji_util.parse = $el => $el;
-
 })();
