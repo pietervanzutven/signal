aa5eca729ad525ba9585ed55be2a4fbbfb93d0ac
Pieter van Zutven <pietervanzutven@outlook.com>
2020-09-01 21:34:06 +0200

    Windows 7 notifications: disable grouping

--- Signal/js/typescript/types/Settings.js
+++ Signal/js/typescript/types/Settings.js
@@ -16,4 +16,7 @@
     const OS = __importStar(window.ts.OS);
     const MIN_WINDOWS_VERSION = '8.0.0';
     exports.isAudioNotificationSupported = () => OS.isWindows(MIN_WINDOWS_VERSION) || OS.isMacOS();
+    // Using `Notification::tag` has a bug on Windows 7:
+    // https://github.com/electron/electron/issues/11189
+    exports.isNotificationGroupingSupported = () => !OS.isWindows() || OS.isWindows(MIN_WINDOWS_VERSION);
 })();
