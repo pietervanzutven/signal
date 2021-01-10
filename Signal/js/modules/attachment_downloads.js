3c81e3eba734533a22d3ffdf743e8bc2e0de9db0
Pieter van Zutven <pietervanzutven@outlook.com>
2021-01-10 11:22:20 +0100

    Ensure out-of-band attachment updates make new top-level objects

--- Signal/js/modules/attachment_downloads.js
+++ Signal/js/modules/attachment_downloads.js
@@ -266,9 +266,6 @@
 
         if (fromConversation && message !== fromConversation) {
           fromConversation.set(message.attributes);
-          fromConversation.trigger('change', fromConversation);
-        } else {
-          message.trigger('change', message);
         }
       }
     }
@@ -318,7 +315,13 @@
           `_addAttachmentToMessage: attachments didn't exist or ${index} was too large`
         );
       }
-      _replaceAttachment(attachments, index, attachment, logPrefix);
+      _checkOldAttachment(attachments, index, attachment, logPrefix);
+
+      const newAttachments = [...attachments];
+      newAttachments[index] = attachment;
+
+      message.set({ attachments: newAttachments });
+
       return;
     }
 
@@ -333,7 +336,19 @@
       if (!item) {
         throw new Error(`_addAttachmentToMessage: preview ${index} was falsey`);
       }
-      _replaceAttachment(item, 'image', attachment, logPrefix);
+
+      _checkOldAttachment(item, 'image', attachment, logPrefix);
+
+      const newPreview = [...preview];
+      newPreview[index] = Object.assign({},
+        preview[index],
+        {
+          image: attachment,
+        }
+      );
+
+      message.set({ preview: newPreview });
+
       return;
     }
 
@@ -346,7 +361,22 @@
       }
       const item = contact[index];
       if (item && item.avatar && item.avatar.avatar) {
-        _replaceAttachment(item.avatar, 'avatar', attachment, logPrefix);
+        _checkOldAttachment(item.avatar, 'avatar', attachment, logPrefix);
+
+        const newContact = [...contact];
+        newContact[index] = Object.assign({},
+          contact[index],
+          {
+            avatar: Object.assign({},
+              contact[index].avatar,
+              {
+                avatar: attachment,
+              }
+            ),
+          }
+        );
+
+        message.set({ contact: newContact });
       } else {
         logger.warn(
           `_addAttachmentToMessage: Couldn't update contact with avatar attachment for message ${message.idForLogging()}`
@@ -371,10 +401,29 @@
       const item = attachments[index];
       if (!item) {
         throw new Error(
-          `_addAttachmentToMessage: attachment ${index} was falsey`
+          `_addAttachmentToMessage: quote attachment ${index} was falsey`
         );
       }
-      _replaceAttachment(item, 'thumbnail', attachment, logPrefix);
+
+      _checkOldAttachment(item, 'thumbnail', attachment, logPrefix);
+
+      const newAttachments = [...attachments];
+      newAttachments[index] = Object.assign({},
+        attachments[index],
+        {
+          thumbnail: attachment,
+        }
+      );
+
+      const newQuote = Object.assign({},
+        quote,
+        {
+          attachments: newAttachments,
+        }
+      );
+
+      message.set({ quote: newQuote });
+
       return;
     }
 
@@ -434,15 +483,15 @@
     );
   }
 
-  function _replaceAttachment(object, key, newAttachment, logPrefix) {
+  function _checkOldAttachment(object, key, newAttachment, logPrefix) {
     const oldAttachment = object[key];
     if (oldAttachment && oldAttachment.path) {
-      logger.warn(
-        `_replaceAttachment: ${logPrefix} - old attachment already had path, not replacing`
+      logger.error(
+        `_checkOldAttachment: ${logPrefix} - old attachment already had path, not replacing`
+      );
+      throw new Error(
+        '_checkOldAttachment: old attachment already had path, not replacing'
       );
     }
-
-    // eslint-disable-next-line no-param-reassign
-    object[key] = newAttachment;
   }
 })();
