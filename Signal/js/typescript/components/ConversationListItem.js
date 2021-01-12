613af27e3e84d7cc3844ea29a549b1057866b6d9
Pieter van Zutven <pietervanzutven@outlook.com>
2021-01-12 21:05:20 +0100

    When showing draft in left pane, don't show last message status

--- Signal/js/typescript/components/ConversationListItem.js
+++ Signal/js/typescript/components/ConversationListItem.js
@@ -50,6 +50,9 @@
             if (!lastMessage && !typingContact) {
                 return null;
             }
+            const showingDraft = shouldShowDraft && draftPreview;
+            // Note: instead of re-using showingDraft here we explode it because
+            //   typescript can't tell that draftPreview is truthy otherwise
             const text = shouldShowDraft && draftPreview
                 ? draftPreview
                 : lastMessage && lastMessage.text
@@ -61,9 +64,9 @@
                         ? 'module-conversation-list-item__message__text--has-unread'
                         : null)
                 }, typingContact ? (react_1.default.createElement(TypingAnimation_1.TypingAnimation, { i18n: i18n })) : (react_1.default.createElement(react_1.default.Fragment, null,
-                    shouldShowDraft ? (react_1.default.createElement("span", { className: "module-conversation-list-item__message__draft-prefix" }, i18n('ConversationListItem--draft-prefix'))) : null,
+                    showingDraft ? (react_1.default.createElement("span", { className: "module-conversation-list-item__message__draft-prefix" }, i18n('ConversationListItem--draft-prefix'))) : null,
                     react_1.default.createElement(MessageBody_1.MessageBody, { text: text, disableJumbomoji: true, disableLinks: true, i18n: i18n })))),
-                lastMessage && lastMessage.status ? (react_1.default.createElement("div", { className: classnames_1.default('module-conversation-list-item__message__status-icon', `module-conversation-list-item__message__status-icon--${lastMessage.status}`) })) : null));
+                !showingDraft && lastMessage && lastMessage.status ? (react_1.default.createElement("div", { className: classnames_1.default('module-conversation-list-item__message__status-icon', `module-conversation-list-item__message__status-icon--${lastMessage.status}`) })) : null));
         }
         render() {
             const { unreadCount, onClick, id, isSelected, style } = this.props;
