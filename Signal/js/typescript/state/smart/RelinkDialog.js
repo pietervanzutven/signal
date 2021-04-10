aa1e62ef7a92cfee51efd5655db93c50b472be20
Pieter van Zutven <pietervanzutven@outlook.com>
2021-04-10 19:13:42 +0200

    Decouple RelinkDialog from NetworkStatusDialog

--- Signal/js/typescript/state/smart/RelinkDialog.js
+++ Signal/js/typescript/state/smart/RelinkDialog.js
@@ -4,17 +4,22 @@
     window.ts = window.ts || {};
     window.ts.state = window.ts.state || {};
     window.ts.state.smart = window.ts.state.smart || {};
-    const exports = window.ts.state.smart.NetworkStatus = {};
+    const exports = window.ts.state.smart.RelinkDialog = {};
 
     Object.defineProperty(exports, "__esModule", { value: true });
     const react_redux_1 = window.react_redux;
     const actions_1 = window.ts.state.actions;
-    const NetworkStatus_1 = window.ts.components.NetworkStatus;
+    const RelinkDialog_1 = window.ts.components.RelinkDialog;
     const user_1 = window.ts.state.selectors.user;
     const network_1 = window.ts.state.selectors.network;
+    const registration_1 = window.ts.util.registration;
     const mapStateToProps = (state) => {
-        return Object.assign(Object.assign({}, state.network), { hasNetworkDialog: network_1.hasNetworkDialog(state), i18n: user_1.getIntl(state) });
+        return {
+            hasNetworkDialog: network_1.hasNetworkDialog(state),
+            i18n: user_1.getIntl(state),
+            isRegistrationDone: registration_1.isDone(),
+        };
     };
     const smart = react_redux_1.connect(mapStateToProps, actions_1.mapDispatchToProps);
-    exports.SmartNetworkStatus = smart(NetworkStatus_1.NetworkStatus);
+    exports.SmartRelinkDialog = smart(RelinkDialog_1.RelinkDialog);
 })();
