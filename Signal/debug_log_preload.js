d5cc5aca1ff0afe498d557c7d0841c4de8831a56
Pieter van Zutven <pietervanzutven@outlook.com>
2021-04-13 17:44:10 +0200

    Debug Log: Copy URL instead of open in browser

--- Signal/debug_log_preload.js
+++ Signal/debug_log_preload.js
@@ -5,6 +5,7 @@
 
     const { ipcRenderer } = window.top.ipc;
     const url = window.top.url;
+    const copyText = window['copy-text-to-clipboard'];
     const i18n = window.top.modules.i18n;
 
     const config = url.parse(window.location.toString(), true).query;
@@ -14,6 +15,7 @@
     window.getVersion = () => config.version;
     window.theme = config.theme;
     window.i18n = i18n.setup(locale, localeMessages);
+    window.copyText = copyText;
 
     window.getUWPVersion = () => config.uwp_version;
     window.getEnvironment = () => config.environment;
