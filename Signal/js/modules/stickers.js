b270a455fd5ac1ea55acbd1d3db0572c6c9bede4
Pieter van Zutven <pietervanzutven@outlook.com>
2021-04-20 18:15:15 +0200

    deleteStickerPackReference: Return early on empty array as well

--- Signal/js/modules/stickers.js
+++ Signal/js/modules/stickers.js
@@ -712,7 +712,7 @@
     const paths = await deleteStickerPackReference(messageId, packId);
 
     // If we don't get a list of paths back, then the sticker pack was not deleted
-    if (!paths) {
+    if (!paths || !paths.length) {
       return;
     }
 
