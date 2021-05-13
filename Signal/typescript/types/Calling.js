1bafab8936513f113a9abc312236c17297bd59fb
Pieter van Zutven <pietervanzutven@outlook.com>
2021-05-13 20:53:32 +0200

    Storybook fix

--- Signal/typescript/types/Calling.js
+++ Signal/typescript/types/Calling.js
@@ -1,8 +1,15 @@
 require(exports => {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    const ringrtc_1 = require("ringrtc");
-    exports.CallState = ringrtc_1.CallState;
+    // Must be kept in sync with RingRTC.CallState
+    var CallState;
+    (function (CallState) {
+        CallState["Prering"] = "init";
+        CallState["Ringing"] = "ringing";
+        CallState["Accepted"] = "connected";
+        CallState["Reconnecting"] = "connecting";
+        CallState["Ended"] = "ended";
+    })(CallState = exports.CallState || (exports.CallState = {}));
     var CallingDeviceType;
     (function (CallingDeviceType) {
         CallingDeviceType[CallingDeviceType["CAMERA"] = 0] = "CAMERA";
