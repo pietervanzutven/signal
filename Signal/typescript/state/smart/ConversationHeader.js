cf6b98cc79b97462822b47ffffee2e69e07e9108
Pieter van Zutven <pietervanzutven@outlook.com>
2021-06-03 17:57:52 +0200

    In conversation header, only show video button for group calls

--- Signal/typescript/state/smart/ConversationHeader.js
+++ Signal/typescript/state/smart/ConversationHeader.js
@@ -11,14 +11,31 @@ require(exports => {
     const conversations_2 = require("../ducks/conversations");
     const calling_1 = require("../ducks/calling");
     const user_1 = require("../selectors/user");
+    const missingCaseError_1 = require("../../util/missingCaseError");
+    const getOutgoingCallButtonStyle = (conversation, state) => {
+        if (calling_1.getActiveCall(state.calling)) {
+            return ConversationHeader_1.OutgoingCallButtonStyle.None;
+        }
+        const conversationCallMode = conversations_2.getConversationCallMode(conversation);
+        switch (conversationCallMode) {
+            case Calling_1.CallMode.None:
+                return ConversationHeader_1.OutgoingCallButtonStyle.None;
+            case Calling_1.CallMode.Direct:
+                return ConversationHeader_1.OutgoingCallButtonStyle.Both;
+            case Calling_1.CallMode.Group:
+                if (!window.GROUP_CALLING) {
+                    return ConversationHeader_1.OutgoingCallButtonStyle.None;
+                }
+                return ConversationHeader_1.OutgoingCallButtonStyle.JustVideo;
+            default:
+                throw missingCaseError_1.missingCaseError(conversationCallMode);
+        }
+    };
     const mapStateToProps = (state, ownProps) => {
         const conversation = conversations_1.getConversationSelector(state)(ownProps.id);
         if (!conversation) {
             throw new Error('Could not find conversation');
         }
-        const conversationCallMode = conversations_2.getConversationCallMode(conversation);
-        const conversationSupportsCalls = conversationCallMode === Calling_1.CallMode.Direct ||
-            (conversationCallMode === Calling_1.CallMode.Group && window.GROUP_CALLING);
         return Object.assign(Object.assign({}, lodash_1.pick(conversation, [
             'acceptedMessageRequest',
             'avatarPath',
@@ -38,7 +55,7 @@ require(exports => {
             'profileName',
             'title',
             'type',
-        ])), { i18n: user_1.getIntl(state), showBackButton: state.conversations.selectedConversationPanelDepth > 0, showCallButtons: conversationSupportsCalls && !calling_1.getActiveCall(state.calling) });
+        ])), { i18n: user_1.getIntl(state), showBackButton: state.conversations.selectedConversationPanelDepth > 0, outgoingCallButtonStyle: getOutgoingCallButtonStyle(conversation, state) });
     };
     const smart = react_redux_1.connect(mapStateToProps, {});
     exports.SmartConversationHeader = smart(ConversationHeader_1.ConversationHeader);
