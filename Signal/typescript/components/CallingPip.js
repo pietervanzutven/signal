53c9f5268365072fbb36297084efabcc73ff10a4
Pieter van Zutven <pietervanzutven@outlook.com>
2021-06-04 21:36:31 +0200

    Prevent calling PiP from going off-screen

--- Signal/typescript/components/CallingPip.js
+++ Signal/typescript/components/CallingPip.js
@@ -7,82 +7,140 @@ require(exports => {
     };
     Object.defineProperty(exports, "__esModule", { value: true });
     const react_1 = __importDefault(require("react"));
+    const lodash_1 = require("lodash");
     const CallingPipRemoteVideo_1 = require("./CallingPipRemoteVideo");
+    const missingCaseError_1 = require("../util/missingCaseError");
+    var PositionMode;
+    (function (PositionMode) {
+        PositionMode[PositionMode["BeingDragged"] = 0] = "BeingDragged";
+        PositionMode[PositionMode["SnapToBottom"] = 1] = "SnapToBottom";
+        PositionMode[PositionMode["SnapToLeft"] = 2] = "SnapToLeft";
+        PositionMode[PositionMode["SnapToRight"] = 3] = "SnapToRight";
+        PositionMode[PositionMode["SnapToTop"] = 4] = "SnapToTop";
+    })(PositionMode || (PositionMode = {}));
     const PIP_HEIGHT = 156;
     const PIP_WIDTH = 120;
-    const PIP_DEFAULT_Y = 56;
+    const PIP_TOP_MARGIN = 56;
     const PIP_PADDING = 8;
     exports.CallingPip = ({ activeCall, getGroupCallVideoFrameSource, hangUp, hasLocalVideo, i18n, setLocalPreview, setRendererCanvas, togglePip, }) => {
         const videoContainerRef = react_1.default.useRef(null);
         const localVideoRef = react_1.default.useRef(null);
-        const [dragState, setDragState] = react_1.default.useState({
-            offsetX: 0,
+        const [windowWidth, setWindowWidth] = react_1.default.useState(window.innerWidth);
+        const [windowHeight, setWindowHeight] = react_1.default.useState(window.innerHeight);
+        const [positionState, setPositionState] = react_1.default.useState({
+            mode: PositionMode.SnapToRight,
             offsetY: 0,
-            isDragging: false,
-        });
-        const [dragContainerStyle, setDragContainerStyle] = react_1.default.useState({
-            translateX: window.innerWidth - PIP_WIDTH - PIP_PADDING,
-            translateY: PIP_DEFAULT_Y,
         });
         react_1.default.useEffect(() => {
             setLocalPreview({ element: localVideoRef });
         }, [setLocalPreview]);
         const handleMouseMove = react_1.default.useCallback((ev) => {
-            if (dragState.isDragging) {
-                setDragContainerStyle({
-                    translateX: ev.clientX - dragState.offsetX,
-                    translateY: ev.clientY - dragState.offsetY,
-                });
+            if (positionState.mode === PositionMode.BeingDragged) {
+                setPositionState(oldState => (Object.assign(Object.assign({}, oldState), { mouseX: ev.screenX, mouseY: ev.screenY })));
             }
-        }, [dragState]);
+        }, [positionState]);
         const handleMouseUp = react_1.default.useCallback(() => {
-            if (dragState.isDragging) {
-                const { translateX, translateY } = dragContainerStyle;
+            if (positionState.mode === PositionMode.BeingDragged) {
+                const { mouseX, mouseY, dragOffsetX, dragOffsetY } = positionState;
                 const { innerHeight, innerWidth } = window;
-                const proximityRatio = {
-                    top: translateY / innerHeight,
-                    right: (innerWidth - translateX) / innerWidth,
-                    bottom: (innerHeight - translateY) / innerHeight,
-                    left: translateX / innerWidth,
-                };
-                const snapTo = Object.keys(proximityRatio).reduce((minKey, key) => {
-                    return proximityRatio[key] < proximityRatio[minKey] ? key : minKey;
-                });
-                setDragState(Object.assign(Object.assign({}, dragState), { isDragging: false }));
-                let nextX = Math.max(PIP_PADDING, Math.min(translateX, innerWidth - PIP_WIDTH - PIP_PADDING));
-                let nextY = Math.max(PIP_DEFAULT_Y, Math.min(translateY, innerHeight - PIP_HEIGHT - PIP_PADDING));
-                if (snapTo === 'top') {
-                    nextY = PIP_DEFAULT_Y;
+                const offsetX = mouseX - dragOffsetX;
+                const offsetY = mouseY - dragOffsetY;
+                const snapCandidates = [
+                    {
+                        mode: PositionMode.SnapToLeft,
+                        distanceToEdge: offsetX,
+                    },
+                    {
+                        mode: PositionMode.SnapToRight,
+                        distanceToEdge: innerWidth - (offsetX + PIP_WIDTH),
+                    },
+                    {
+                        mode: PositionMode.SnapToTop,
+                        distanceToEdge: offsetY - PIP_TOP_MARGIN,
+                    },
+                    {
+                        mode: PositionMode.SnapToBottom,
+                        distanceToEdge: innerHeight - (offsetY + PIP_HEIGHT),
+                    },
+                ];
+                // This fallback is mostly for TypeScript, because `minBy` says it can return
+                //   `undefined`.
+                const snapTo = lodash_1.minBy(snapCandidates, candidate => candidate.distanceToEdge) ||
+                    snapCandidates[0];
+                switch (snapTo.mode) {
+                    case PositionMode.SnapToLeft:
+                    case PositionMode.SnapToRight:
+                        setPositionState({
+                            mode: snapTo.mode,
+                            offsetY,
+                        });
+                        break;
+                    case PositionMode.SnapToTop:
+                    case PositionMode.SnapToBottom:
+                        setPositionState({
+                            mode: snapTo.mode,
+                            offsetX,
+                        });
+                        break;
+                    default:
+                        throw missingCaseError_1.missingCaseError(snapTo.mode);
                 }
-                if (snapTo === 'right') {
-                    nextX = innerWidth - PIP_WIDTH - PIP_PADDING;
-                }
-                if (snapTo === 'bottom') {
-                    nextY = innerHeight - PIP_HEIGHT - PIP_PADDING;
-                }
-                if (snapTo === 'left') {
-                    nextX = PIP_PADDING;
-                }
-                setDragContainerStyle({
-                    translateX: nextX,
-                    translateY: nextY,
-                });
             }
-        }, [dragState, dragContainerStyle]);
+        }, [positionState, setPositionState]);
         react_1.default.useEffect(() => {
-            if (dragState.isDragging) {
+            if (positionState.mode === PositionMode.BeingDragged) {
                 document.addEventListener('mousemove', handleMouseMove, false);
                 document.addEventListener('mouseup', handleMouseUp, false);
+                return () => {
+                    document.removeEventListener('mouseup', handleMouseUp, false);
+                    document.removeEventListener('mousemove', handleMouseMove, false);
+                };
             }
-            else {
-                document.removeEventListener('mouseup', handleMouseUp, false);
-                document.removeEventListener('mousemove', handleMouseMove, false);
-            }
+            return lodash_1.noop;
+        }, [positionState.mode, handleMouseMove, handleMouseUp]);
+        react_1.default.useEffect(() => {
+            const handleWindowResize = lodash_1.debounce(() => {
+                setWindowWidth(window.innerWidth);
+                setWindowHeight(window.innerHeight);
+            }, 100, {
+                maxWait: 3000,
+            });
+            window.addEventListener('resize', handleWindowResize, false);
             return () => {
-                document.removeEventListener('mouseup', handleMouseUp, false);
-                document.removeEventListener('mousemove', handleMouseMove, false);
+                window.removeEventListener('resize', handleWindowResize, false);
             };
-        }, [dragState, handleMouseMove, handleMouseUp]);
+        }, []);
+        const [translateX, translateY] = react_1.default.useMemo(() => {
+            switch (positionState.mode) {
+                case PositionMode.BeingDragged:
+                    return [
+                        positionState.mouseX - positionState.dragOffsetX,
+                        positionState.mouseY - positionState.dragOffsetY,
+                    ];
+                case PositionMode.SnapToLeft:
+                    return [
+                        PIP_PADDING,
+                        Math.min(PIP_TOP_MARGIN + positionState.offsetY, windowHeight - PIP_PADDING - PIP_HEIGHT),
+                    ];
+                case PositionMode.SnapToRight:
+                    return [
+                        windowWidth - PIP_PADDING - PIP_WIDTH,
+                        Math.min(PIP_TOP_MARGIN + positionState.offsetY, windowHeight - PIP_PADDING - PIP_HEIGHT),
+                    ];
+                case PositionMode.SnapToTop:
+                    return [
+                        Math.min(positionState.offsetX, windowWidth - PIP_PADDING - PIP_WIDTH),
+                        PIP_TOP_MARGIN + PIP_PADDING,
+                    ];
+                case PositionMode.SnapToBottom:
+                    return [
+                        Math.min(positionState.offsetX, windowWidth - PIP_PADDING - PIP_WIDTH),
+                        windowHeight - PIP_PADDING - PIP_HEIGHT,
+                    ];
+                default:
+                    throw missingCaseError_1.missingCaseError(positionState);
+            }
+        }, [windowWidth, windowHeight, positionState]);
         return (
             // eslint-disable-next-line jsx-a11y/no-static-element-interactions
             react_1.default.createElement("div", {
@@ -92,17 +150,23 @@ require(exports => {
                         return;
                     }
                     const rect = node.getBoundingClientRect();
-                    const offsetX = ev.clientX - rect.left;
-                    const offsetY = ev.clientY - rect.top;
-                    setDragState({
-                        isDragging: true,
-                        offsetX,
-                        offsetY,
+                    const dragOffsetX = ev.screenX - rect.left;
+                    const dragOffsetY = ev.screenY - rect.top;
+                    setPositionState({
+                        mode: PositionMode.BeingDragged,
+                        mouseX: ev.screenX,
+                        mouseY: ev.screenY,
+                        dragOffsetX,
+                        dragOffsetY,
                     });
                 }, ref: videoContainerRef, style: {
-                    cursor: dragState.isDragging ? '-webkit-grabbing' : '-webkit-grab',
-                    transform: `translate3d(${dragContainerStyle.translateX}px,${dragContainerStyle.translateY}px, 0)`,
-                    transition: dragState.isDragging ? 'none' : 'transform ease-out 300ms',
+                    cursor: positionState.mode === PositionMode.BeingDragged
+                        ? '-webkit-grabbing'
+                        : '-webkit-grab',
+                    transform: `translate3d(${translateX}px,${translateY}px, 0)`,
+                    transition: positionState.mode === PositionMode.BeingDragged
+                        ? 'none'
+                        : 'transform ease-out 300ms',
                 }
             },
                 react_1.default.createElement(CallingPipRemoteVideo_1.CallingPipRemoteVideo, { activeCall: activeCall, getGroupCallVideoFrameSource: getGroupCallVideoFrameSource, i18n: i18n, setRendererCanvas: setRendererCanvas }),
