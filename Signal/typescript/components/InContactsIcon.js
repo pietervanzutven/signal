e9a9978af01c4a0b4597100ec109329b71c5f28e
Pieter van Zutven <pietervanzutven@outlook.com>
2021-06-11 18:16:30 +0200

    Fix layout of "this person is in your contacts" tooltip

--- Signal/typescript/components/InContactsIcon.js
+++ Signal/typescript/components/InContactsIcon.js
@@ -1,6 +1,6 @@
 require(exports => {
     "use strict";
-    // Copyright 2020 Signal Messenger, LLC
+    // Copyright 2020-2021 Signal Messenger, LLC
     // SPDX-License-Identifier: AGPL-3.0-only
     var __importDefault = (this && this.__importDefault) || function (mod) {
         return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -12,9 +12,8 @@ require(exports => {
     exports.InContactsIcon = (props) => {
         const { className, i18n } = props;
         /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-        return (react_1.default.createElement("span", { className: "module-in-contacts-icon__tooltip" },
-            react_1.default.createElement(Tooltip_1.Tooltip, { content: i18n('contactInAddressBook') },
-                react_1.default.createElement("span", { "aria-label": i18n('contactInAddressBook'), className: classnames_1.default('module-in-contacts-icon__icon', className), role: "img", tabIndex: 0 }))));
+        return (react_1.default.createElement(Tooltip_1.Tooltip, { content: i18n('contactInAddressBook') },
+            react_1.default.createElement("span", { "aria-label": i18n('contactInAddressBook'), className: classnames_1.default('module-in-contacts-icon__icon', className), role: "img", tabIndex: 0 })));
         /* eslint-enable jsx-a11y/no-noninteractive-tabindex */
     };
 });
